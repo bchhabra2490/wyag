6512a40e4f91e2ad9f38f0a20c3fb2fc1a567bee
Bharat Chhabra <bchhabra2490@gmail.com>
2025-11-03 14:32:53 +0530

    Status cmd

--- libwyag.py
+++ libwyag.py
@@ -852,7 +852,7 @@ def ref_resolve(repo, ref):
         return None
 
     with open(path, "r") as f:
-        data = fp.read()[:-1]
+        data = f.read()[:-1]
 
     if data.startswith("ref: "):
         return ref_resolve(repo, data[5:])
@@ -1094,3 +1094,82 @@ def cmd_status_branch(repo):
         print(f"On branch {branch}.")
     else:
         print(f"Head detached at {object_find(repo, 'HEAD')}")
+
+
+def tree_to_dict(repo, ref, prefix=""):
+    ret = dict()
+    tree_sha = object_find(repo, ref, fmt=b"tree")
+    tree = object_read(repo, tree_sha)
+
+    for leaf in tree.items:
+        full_path = os.path.join(prefix, leaf.path)
+        is_subtree = leaf.mode.startswith(b"04")
+
+        if is_subtree:
+            ret.update(tree_to_dict(repo, leaf.sha, full_path))
+        else:
+            ret[full_path] = leaf.sha
+
+    return ret
+
+
+def cmd_status_head_index(repo, index):
+    print("Changes to be committed:")
+
+    head = tree_to_dict(repo, "HEAD")
+    for entry in index.entries:
+        if entry.name in head:
+            if head[entry.name] != entry.sha:
+                print(" modified:", entry.name)
+            del head[entry.name]
+        else:
+            print(" added: ", entry.name)
+
+    for entry in head.keys():
+        print(" deleted: ", entry)
+
+
+def cmd_status_index_worktree(repo, index):
+    print("Changes not staged for commit:")
+
+    ignore = gitignore_read(repo)
+
+    gitdir_prefix = repo.gitdir + os.path.sep
+
+    all_files = list()
+
+    for root, _, files in os.walk(repo.worktree, True):
+        if repo == repo.gitdir or root.startswith(gitdir_prefix):
+            continue
+        for f in files:
+            full_path = os.path.join(root, f)
+            rel_path = os.path.relpath(full_path, repo.worktree)
+            all_files.append(rel_path)
+
+    for entry in index.entries:
+        full_path = os.path.join(repo.worktree, entry.name)
+        if not os.path.exists(full_path):
+            print(" deleted: ", entry.name)
+        else:
+            stat = os.stat(full_path)
+
+            ctime_ns = entry.ctime[0] * 10**9 + entry.ctime[1]
+            mtime_ns = entry.mtime[0] * 10**9 + entry.mtime[1]
+
+            if (stat.st_ctime_ns != ctime_ns) or (stat.st_mtime_ns != mtime_ns):
+                with open(full_path, "rb") as fd:
+                    new_sha = object_hash(fd, b"blob", None)
+
+                    same = entry.sha == new_sha
+                    if not same:
+                        print(" modified: ", entry.name)
+
+        if entry.name in all_files:
+            all_files.remove(entry.name)
+
+    print()
+    print("Untracked files:")
+
+    for f in all_files:
+        if not check_ignore(ignore, f):
+            print(" ", f)
